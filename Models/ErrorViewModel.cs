9f374e86dea7ffe53b58c83bba0251e84a252aa3
juanmartinspd <juanfigueiredom@gmail.com>
2024-12-09 20:23:44 -0300

    Adicionar arquivos de projeto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Web.MeuPortifolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
